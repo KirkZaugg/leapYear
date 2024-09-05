5b7d6eff412b722c435a5b3a71433543974205eb
KirkZaugg <25kzaugg@go.dsdmail.net>
2024-09-05 14:55:07 +0000

    x

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+bool isLeapYear(int year);
+
+int main() {
+    int year;
+    std::cin << year;
+    std::string ans = 
+    return 0;
+}
+
+bool isLeapYear(int year) {
+
+}
